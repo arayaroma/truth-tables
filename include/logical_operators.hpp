fec320081271641319309b0296a603187b558814
Daniel Araya <darayaroma@gmail.com>
2023-08-26 18:36:30 -0600

    [add] constants class for logical_operators

---
+++ include/logical_operators.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+class LogicalOperators {
+public:
+  static const char AND;
+  static const char OR;
+  static const char NOT;
+  static const char XOR;
+  static const char WHITE_SPACE;
+  static const char LEFT_PARENTHESIS;
+  static const char RIGHT_PARENTHESIS;
+  static const char COMMA;
+};
---
+++ src/logical_operators.cpp
@@ -0,0 +1,10 @@
+#include "logical_operators.hpp"
+
+const char LogicalOperators::AND = '*';
+const char LogicalOperators::OR = '+';
+const char LogicalOperators::NOT = '\'';
+const char LogicalOperators::XOR = '#';
+const char LogicalOperators::WHITE_SPACE = ' ';
+const char LogicalOperators::LEFT_PARENTHESIS = '(';
+const char LogicalOperators::RIGHT_PARENTHESIS = ')';
+const char LogicalOperators::COMMA = ',';
